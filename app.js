df895c7c30317b48fd7d198ef780959d1019a503
RaulLlamas <rauldllamas@gmail.com>
2021-07-04 01:07:42 -0500

    Todo

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path =require('path');
+
+const app = express();
+
+const publicPath = path.resolve(__dirname,'./public');
+app.use(express.static(publicPath));
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, ()=> {
+    console.log('Servidor corriendo en el puerto' + PORT)
+});
+
+app.get('/', (req,res)=> {
+    res.sendFile(path.resolve(__dirname,'./views/home.html'));
+})
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
